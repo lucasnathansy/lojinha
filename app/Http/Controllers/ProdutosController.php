1d07e8f0c7dd5451b104020213b7c39876b54168
lucasnathansy <lnathan2014@gmail.com>
2023-08-30 06:28:01 -0300

    up

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
+
 use Illuminate\Http\Request;
 
 class ProdutosController extends Controller
 {
-    public function index (){
-        return 'produtos';
+    public function index()
+    {
+        $findProduto = Produto::all();
+        return view('pages.produtos.paginacao', compact('findProduto'));
     }
 }
